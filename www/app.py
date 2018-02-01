3f6317c3e7bddb90628011cfc32113d018c6b92d
lvmaple <lvmaple9527@163.com>
2018-02-01 18:06:04 +0800

    0201

--- www/app.py
+++ www/app.py
@@ -1,9 +1,98 @@
-import logging; logging.basicConfig(level=logging.INFO)
-
+import logging;
 import asyncio, os, json, time
 from datetime import datetime
 
-from aiohttp import  web
+from aiohttp import web
+from jinja2 import Environment, FileSystemLoader
+
+import orm
+from coroweb import add_routes, add_static
+
+logging.basicConfig(level=logging.INFO)
+
+
+def init_jinja2(app, **kw):
+    logging.info('init jija2....')
+    options = dict(
+        autoescape=kw.get('autoescape', True),
+        block_start_string=kw.get('block_start_string', '{%'),
+        block_end_string=kw.get('block_end_string', '%}'),
+        variable_start_string=kw.get('variable_start_string', '{{'),
+        variable_end_string=kw.get('variable_end_string', '}}'),
+        auto_reload=kw.get('auto_reload', True)
+    )
+    path = kw.get('path', None)
+    if path is None:
+        path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+    logging.info('set jinja2 template path: {!s}'.format(path))
+    env = Environment(loader=FileSystemLoader(path), **options)
+    filters = kw.get('filters', None)
+    if filters is not None:
+        for name, f in filters.items():
+            env.filters[name] = f
+    app['__templating__'] = env
+
+
+async def logger_factory(app, handler):
+    async def logger(request):
+        logging.info('Request: {!s} {!s}'.format(request.method, request.path))
+        return (await handler(request))
+
+    return logger
+
+
+async def data_factory(app, handler):
+    async def parse_data(request):
+        if request.content_type.startswith('application/json'):
+            request.__data__ = await request.json()
+            logging.info('request json: {!s}'.format(str(request.__data__)))
+        elif request.content_type.startswith('application/x-www-form-urlencoded'):
+            request.__data__ = await  request.post()
+            logging.info('request form: {!s}'.format(request.__data__))
+        return (await handler(request))
+
+    return parse_data
+
+
+async def response_factory(app, handler):
+    async def response(request):
+        logging.info('Response handler...')
+        r = await handler(request)
+        if isinstance(r, web.StreamResponse):
+            return r
+        if isinstance(r, bytes):
+            resp = web.Response(body=r)
+            resp.content_type = 'application/octet-stream'
+            return resp
+        if isinstance(r, str):
+            if r.startswith('redirect:'):
+                return web.HTTPFound(r[9:])
+            resp = web.Response(body=r.encode('utf-8'))
+            resp.content_type = 'text/html;charset=utf-8'
+            return resp
+        if isinstance(r, dict):
+            template = r.get('__template__')
+            if template is None:
+                resp = web.Response(
+                    body=json.dumps(r, ensure_ascii=False, default=lambda o: o.__dict__).encode('utf-8'))
+                resp.content_type = 'application/json;charset=utf-8'
+                return resp
+            else:
+                resp = web.Response(body=app['__templating__'].get_template(template).render(**r).encode('utf-8'))
+                resp.content_type = 'text/html;charset=utf-8'
+                return resp
+        if isinstance(r, int) and r >= 100 and r < 600:
+            return web.Response(r)
+        if isinstance(r, tuple) and len(r) == 2:
+            t, m = r
+            if isinstance(t, int) and t >= 100 and t < 600:
+                return web.Response(t, str(m))
+        # default:
+        resp = web.Response(body=str(r).encode('utf-8'))
+        resp.content_type = 'text/plain;charset=utf-8'
+        return resp
+
+    return response
 
 
 def index(request):
@@ -21,6 +110,6 @@ async def init(loop):
     return srv
 
 
-loop = asyncio .get_event_loop()
+loop = asyncio.get_event_loop()
 loop.run_until_complete(init(loop))
 loop.run_forever()
